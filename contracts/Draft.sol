a784c26c518e7637e5879a5fc7c7c83bec853336
Ambrose Hundal <amshunda@ucsc.edu>
2019-01-11 23:58:56 -0800

    function to draft players

--- contracts/Draft.sol
+++ contracts/Draft.sol
@@ -2,6 +2,13 @@ pragma ^0.5.0;
 
 contract Draft{
 
+address[32] public team_owners;
+
+//Draft a player
+
+function draft(uint playerId) public returns (uint){
+    require(playerId >= 0 && playerId <= 31);
+}
     
 
 }
