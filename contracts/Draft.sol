19ecf9070f1c57d94565f4f482b7ee3d84028659
Ambrose Hundal <amshunda@ucsc.edu>
2019-01-12 15:08:32 -0800

    modifier and variables created for player trades

--- contracts/Draft.sol
+++ contracts/Draft.sol
@@ -4,10 +4,21 @@ contract Draft{
 
 address[32] public team_owners;
 
+//mapping of SB rings
+mapping(address => uint) public superbowl_rings;
+
+
 //Draft a player
 
 function draft(uint playerId) public returns (uint){
     require(playerId >= 0 && playerId <= 31);
+}
+
+//Modifier to restrict trade only between team_owners
+
+modifier onlyOwner(address _owner){
+    require(msg.sender == _owner);
+    _;
 }
     
 
