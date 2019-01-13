ac48901403270f75e5c7f2fbfc11a7abe25b195c
Ambrose Hundal <amshunda@ucsc.edu>
2019-01-12 16:46:36 -0800

    player stats added struct

--- contracts/Draft.sol
+++ contracts/Draft.sol
@@ -7,6 +7,20 @@ address[32] public team_owners;
 //mapping of SB rings
 mapping(address => uint) public superbowl_rings;
 
+//struct for best game stats
+
+struct PlayerStat{
+    uint touchdowns;
+    uint reception_yards;
+    uint rushing_yards;
+    uint games;
+    string name;
+
+} 
+
+//mapping of best 32 players game stat
+mapping(address => PlayerStat[32]) best_season_stats;
+
 
 //Draft a player
 
