3e4a9c8da9fc8f3ab83ec71b46316860775c79b7
Ambrose Hundal <amshunda@ucsc.edu>
2019-01-14 23:32:30 -0800

    added attributes to playerstat struct

--- contracts/Draft.sol
+++ contracts/Draft.sol
@@ -15,6 +15,8 @@ struct PlayerStat{
     uint rushing_yards;
     uint games;
     string name;
+    bool winSB_next_year;
+    uint overall_skill_level;
 } 
 
 
@@ -22,15 +24,15 @@ struct PlayerStat{
 mapping(address => PlayerStat[32]) best_season_stats;
 
 //function to insert player
-function insertStat(address playerAddress, uint tds, uint rush, uint recs, uint games, string n) public returns (bool success){
+function insertStat(address playerAddress, uint tds, uint rush, uint recs, uint games, uint skillLevel, string n) public returns (bool success){
     best_season_stats[playerAddress].touchdowns = tds;
     best_season_stats[playerAddress].rushing_yards = rush;
     best_season_stats[playerAddress].reception_yards = recs;
     best_season_stats[playerAddress].games = games;
     best_season_stats[playerAddress].name = n;
+    best_season_stats[playerAddress].overall_skill_level = skillLevel;
 
     return true;
-
 }
 
 
