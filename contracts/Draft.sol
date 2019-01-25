9dfa35a5c511fc00fb23de0e31bbe478c65c2cb4
Ambrose Hundal <amshunda@ucsc.edu>
2019-01-24 17:58:23 -0800

    new functions added

--- contracts/Draft.sol
+++ contracts/Draft.sol
@@ -8,51 +8,17 @@ address[32] public team_owners;
 //mapping of SB rings
 mapping(address => uint) public superbowl_rings;
 
-//struct for best game stats
 
-struct PlayerStat{
-    uint touchdowns;
-    uint reception_yards;
-    uint rushing_yards;
-    uint games;
-    string name;
-    bool winSB_next_year;
-    uint overall_skill_level;
-} 
-
-//player skills levels
-   
-    uint constant amateur = 1;
-    uint constant pro = 2;
-    uint constant elite = 3;
-    uint constant hof = 4;
-
-
-
-//mapping of best 32 players game stat
-mapping(address => PlayerStat[32]) best_season_stats;
-
-//function to insert player
-function insertStat(address playerAddress, uint tds, uint rush, uint recs, uint games, uint skillLevel, string n) public returns (bool success){
-    best_season_stats[playerAddress].touchdowns = tds;
-    best_season_stats[playerAddress].rushing_yards = rush;
-    best_season_stats[playerAddress].reception_yards = recs;
-    best_season_stats[playerAddress].games = games;
-    best_season_stats[playerAddress].name = n;
-    best_season_stats[playerAddress].overall_skill_level = skillLevel;
-
-    return true;
-}
-
-function showStats(address player) public view returns(struct){
-    return 
-}
 
 
 //Draft a player
 
 function draft(uint playerId) public returns (uint){
     require(playerId >= 0 && playerId <= 31);
+
+    team_owners[playerId] = msg.sender;
+
+    return playerId;
 }
 
 //Modifier to restrict trade only between team_owners
@@ -60,6 +26,11 @@ function draft(uint playerId) public returns (uint){
 modifier onlyOwner(address _owner){
     require(msg.sender == _owner);
     _;
+}
+
+//retreiving the team team owners 
+function getOwners() public view returns (address[32] memory){
+    return team_owners;
 }
     
 
